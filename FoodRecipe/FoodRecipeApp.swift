13fc24296d484d2dbbf95bc0ce5654eef976f547
RaulPalade <raulpalade@outlook.com>
2024-05-14 11:43:32 +0200

    Initial Commit

---
+++ FoodRecipe/FoodRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodRecipeApp.swift
+//  FoodRecipe
+//
+//  Created by Raul Palade on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
