a01f2ae1360bcac6a06978e1908b480584b86f93
Raul Palade <raulpalade@outlook.com>
2024-05-17 12:23:22 +0200

    [ADD] Assets

--- FoodRecipe/Views/Auth/LoginView.swift
+++ FoodRecipe/Views/Auth/LoginView.swift
@@ -16,128 +16,121 @@ struct LoginView: View {
 
     var body: some View {
         NavigationView {
-            VStack {
-                Spacer()
-                VStack(spacing: 12) {
-                    Text("Sign in now")
-                        .font(.custom("Cabin-Regular", size: 26))
-                        .padding([.leading, .trailing], 16)
-                        .foregroundColor(Color("ButtonColor"))
-                        .multilineTextAlignment(.center)
-                        .bold()
-
-                    Text("Please sign in to continue our app")
-                        .font(.custom("Cabin-Regular", size: 16))
-                        .padding([.leading, .trailing], 16)
-                        .foregroundColor(Color("ButtonColor"))
-                        .multilineTextAlignment(.center)
-                }
-
-                VStack(spacing: 24) {
-                    TextField("Email", text: $email)
-                        .padding(.all, 24)
-                        .foregroundColor(Color("ButtonColor"))
-                        .background(Color("LabelColor"))
-                        .clipShape(RoundedRectangle(cornerRadius: 16))
-
-                    HStack {
-                        if isSecure {
-                            SecureField("Password", text: $password)
-                        } else {
-                            TextField("Password", text: $password)
-                        }
-                        Button(action: {
-                            isSecure.toggle()
-                        }) {
-                            Image(systemName: isSecure ? "eye.slash.fill" : "eye.fill")
-                                .foregroundColor(Color("ButtonColor"))
-                        }
-                    }
-                    .padding(.all, 24)
-                    .background(Color("LabelColor"))
-                    .clipShape(RoundedRectangle(cornerRadius: 16))
-
-                    HStack {
+            ZStack {
+                GeometryReader { geometry in
+                    VStack {
                         Spacer()
-                        NavigationLink {
-                            ForgotPasswordView()
-                        } label: {
-                            HStack(spacing: 4) {
-                                Text("Forgot Password?")
-                                    .font(.custom("Cabin-Regular", size: 18))
-                                    .bold()
-                                    .foregroundColor(Color("ButtonColor"))
-                                    .padding(.vertical, 18)
-                                    .padding(.trailing, 8)
+                        Image("bottom")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: geometry.size.width)
+                    }
+                }
+                VStack {
+                    Spacer()
+                    Image("logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 120, height: 120)
+                    VStack(spacing: 12) {
+                        Text("Sign in now")
+                            .font(.custom("Cabin-Regular", size: 26))
+                            .padding([.leading, .trailing], 16)
+                            .foregroundColor(Color("ButtonColor"))
+                            .multilineTextAlignment(.center)
+                            .bold()
+                        Text("Please sign in to continue our app")
+                            .font(.custom("Cabin-Regular", size: 16))
+                            .padding([.leading, .trailing], 16)
+                            .foregroundColor(Color("ButtonColor"))
+                            .multilineTextAlignment(.center)
+                    }
+                    .padding(.bottom, 20)
+
+                    VStack(spacing: 24) {
+                        TextField("Email", text: $email)
+                            .padding(.all, 24)
+                            .foregroundColor(Color("ButtonColor"))
+                            .background(Color("AppLabelColor"))
+                            .clipShape(RoundedRectangle(cornerRadius: 16))
+                        VStack {
+                            HStack {
+                                if isSecure {
+                                    SecureField("Password", text: $password)
+                                } else {
+                                    TextField("Password", text: $password)
+                                }
+                                Button(action: {
+                                    isSecure.toggle()
+                                }) {
+                                    Image(systemName: isSecure ? "eye.slash.fill" : "eye.fill")
+                                        .foregroundColor(Color("ButtonColor"))
+                                }
+                            }
+                            .padding(.all, 24)
+                            .background(Color("AppLabelColor"))
+                            .clipShape(RoundedRectangle(cornerRadius: 16))
+                            HStack {
+                                Spacer()
+                                NavigationLink {
+                                    ForgotPasswordView()
+                                } label: {
+                                    HStack(spacing: 4) {
+                                        Text("Forgot Password?")
+                                            .font(.custom("Cabin-Regular", size: 18))
+                                            .bold()
+                                            .foregroundColor(Color("ButtonColor"))
+                                            .padding(.vertical, 18)
+                                            .padding(.trailing, 8)
+                                    }
+                                }
+                                .buttonStyle(.borderless)
                             }
                         }
-                        .buttonStyle(.borderless)
-                        .foregroundColor(.white)
-                    }
-
-                }.padding([.horizontal], 24)
-
-                VStack {
-                    Button(action: signIn) {
-                        Text("Login")
-                            .font(.custom("Cabin-Regular", size: 18))
-                            .bold()
-                            .padding(.vertical, 18)
-                            .frame(maxWidth: .infinity)
-                    }
-                    .buttonStyle(.borderedProminent)
-                    .tint(Color("ButtonColor"))
-                    .padding(.horizontal, 24)
-                    .foregroundColor(.white)
-
-                    HStack {
-                        Text("Don't have an account?")
-                            .font(.custom("Cabin-Regular", size: 16))
-                            .foregroundColor(Color(.gray))
-                            .padding(.vertical, 16)
-
-                        NavigationLink {
-                            RegisterView()
-                        } label: {
-                            Text("Sign up")
-                                .font(.custom("Cabin-Regular", size: 16))
-                                .bold()
-                                .foregroundColor(Color("ButtonColor"))
-                                .padding(.vertical, 16)
+                        VStack {
+                            Button(action: signIn) {
+                                Text("Login")
+                                    .font(.custom("Cabin-Regular", size: 18))
+                                    .bold()
+                                    .padding(.vertical, 18)
+                                    .frame(maxWidth: .infinity)
+                            }
+                            .buttonStyle(.borderedProminent)
+                            .tint(Color("ButtonColor"))
+                            .foregroundColor(.white)
+                            HStack {
+                                Text("Don't have an account?")
+                                    .font(.custom("Cabin-Regular", size: 16))
+                                    .foregroundColor(Color(.gray))
+                                    .padding(.vertical, 16)
+                                NavigationLink {
+                                    RegisterView()
+                                } label: {
+                                    Text("Sign up")
+                                        .font(.custom("Cabin-Regular", size: 16))
+                                        .bold()
+                                        .foregroundColor(Color("ButtonColor"))
+                                        .padding(.vertical, 16)
+                                }
+                                .buttonStyle(.borderless)
+                                .foregroundColor(.white)
+                            }
+                            .padding(.horizontal, 24)
                         }
-                        .buttonStyle(.borderless)
-                        .foregroundColor(.white)
                     }
-                    .padding(.horizontal, 24)
-                }
-                .padding(.top, 40)
-
-                Spacer()
-
-                HStack(spacing: 20) {
-                    Image("icon_facebook")
-                        .resizable()
-                        .frame(width: 44, height: 44)
-                        .aspectRatio(contentMode: .fit)
-                    Image("icon_instagram")
-                        .resizable()
-                        .frame(width: 44, height: 44)
-                        .aspectRatio(contentMode: .fit)
-                    Image("icon_twitter")
-                        .resizable()
-                        .frame(width: 44, height: 44)
-                        .aspectRatio(contentMode: .fit)
+                    .padding([.horizontal], 24)
+                    .padding(.bottom, 40)
+                    Spacer()
                 }
             }
-            .padding(.bottom, 70)
             .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .edgesIgnoringSafeArea(.all)
-            .background(Color(.white))
+            .ignoresSafeArea(.all)
         }
-        .navigationDestination(
-            isPresented: $isLoggingIn) {
-                MainView()
+        .navigationDestination(isPresented: $isLoggingIn) {
+            MainView()
         }
+        .navigationBarBackButtonHidden(true)
+        .navigationBarHidden(true)
     }
 
     func signIn() {
