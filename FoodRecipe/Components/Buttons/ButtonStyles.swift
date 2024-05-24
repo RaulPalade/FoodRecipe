25e97775d6cbe181c9aca6887c098df0aa16f53c
Raul Palade <raulpalade@outlook.com>
2024-05-24 12:42:18 +0200

    [FIX] Code clening in recipe detail view

---
+++ FoodRecipe/Components/Buttons/ButtonStyles.swift
@@ -0,0 +1,8 @@
+//
+//  ButtonStyles.swift
+//  FoodRecipe
+//
+//  Created by Raul Palade on 24/05/24.
+//
+
+import Foundation
---
+++ FoodRecipe/Components/VisualEffectView.swift
@@ -0,0 +1,18 @@
+//
+//  VisualEffectView.swift
+//  FoodRecipe
+//
+//  Created by Raul Palade on 24/05/24.
+//
+
+import SwiftUI
+
+struct VisualEffectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VisualEffectView()
+}
